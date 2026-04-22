06e3b1c29db2dc53958e59153fed5e61e980a364
FaiquaNaeem <faiquanaeem07@gmail.com>
2026-04-23 01:18:47 +0530

    fixed api base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-fsd-mse2-ik72.onrender.com"
+  baseURL: "https://ai-fsd-mse2-ik72.onrender.com/api"
 });
 
 export default API;
